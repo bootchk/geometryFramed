d8923c01ed623f5108fc22f8e78ab0901e9a5b75
Lloyd Konneker <bootch@nc.rr.com>
2013-08-20 11:39:38 -0400

    testing setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='geometryFramed',
+      version='0.01',
+      description='2D geometry enforcing frame safety',
+      author='Lloyd K. Konneker',
+      author_email='bootch@nc.rr.com',
+      url='https://github.com/bootchk/geometryFramed',
+      license='LGPLv3+',
+      packages=['geometryFramed',],
+      )
